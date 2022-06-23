424f0826cfde5bd1049ffeab54e754aead4eed0d
Paola Luna <iris.luna@uned.cr>
2022-06-23 15:27:56 -0600

    cree componentes y servicio

--- APIangular/src/environments/environment.ts
+++ APIangular/src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  giphyApikey:'vNInb7OoVXOrmpxc1Opo8KLUXFtnm7TY'
 };
 
 /*
