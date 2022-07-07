684e2b38077f00f490e9e436bf0ee3a7cc15530b
Paola Luna <iris.luna@uned.cr>
2022-07-07 10:27:32 -0600

    cree el nav y el servicio

--- Proyecto-final/src/app/app.module.ts
+++ Proyecto-final/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { TarjetaComponent } from './tarjeta/tarjeta.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    TarjetaComponent
   ],
   imports: [
     BrowserModule,
